e4353729db5940f9894f946fbe7d17b7262f4800
mcoam <miguelcoam@gmail.com>
2018-06-24 22:06:39 -0400

    first commit

--- zDeleteEmail.pl
+++ zDeleteEmail.pl
@@ -7,8 +7,10 @@ my @email_id = '';
 
 ##Example: mcoa@example.com - DD/MM/YY - /Inbox
 my ($account, $date, $folder) = @ARGV;
+    open(FILE, ">/tmp/report_$account") or die "Cannot open file";
     @email_id = `$zmmailbox $account search -l 1000 "in:$folder (before:$date)" | grep conv | awk {'print \$2'} |sed s/\-//g`;
     for my $id (@email_id) {
-        print "Delete email with ID: $id";
+        printf FILE "Delete email with ID: $id";
         system("$zmmailbox $account dm $id");
     }
+close(FILE);
